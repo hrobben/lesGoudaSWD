97400439e495208d7cd2df9f70d9400201b77876
sys72740hro <Hcbdag@635>
2022-06-16 09:32:39 +0200

    Voorbeedl 2

--- index.php
+++ index.php
@@ -5,3 +5,5 @@ echo '<H1>Dit is de eerste tekst voor github.</H1>';
 echo '<p>Groeten van Henry Robben</p><br>';
 
 echo '<p><b><i>phpinfo()</i> is verwijderd en vervangen door deze tekst</b></p>';
+
+echo '<p>dit word een nieuwe branch voor klas swd1 en dient dus alleen als voorbeeld om een branch te maken.</p>';
